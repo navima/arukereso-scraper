68e9561b0a5799961af4d839c648b50394cb8b1c
navima <draco.109@gmail.com>
2022-03-28 20:21:58 +0200

    Added dry-run and logging

--- notify.ps1
+++ notify.ps1
@@ -1,38 +1,51 @@
 param (
     [string]$MAILGUN_API_KEY = '',
     [string]$WATCHLIST_SOURCE = '',
-    $csv_file = ''
+    $csv_file = '',
+    [boolean]$send_email = $true
 )
 
-echo $csv_file
+Write-Output "Filename: $csv_file"
 
 $csv = Import-Csv $csv_file
 $prices = @{}
-$csv | % {
+$csv | ForEach-Object {
     $prices.Add($_.Name, $_.Value)
 }
 
-$watcherListJson = curl $WATCHLIST_SOURCE
+Write-Output ("Rows: " + $prices.Count)
+
+$watcherListJson = curl -s $WATCHLIST_SOURCE
 $watcherlist = $watcherListJson | ConvertFrom-Json 
 
-$watcherlist | % {
-    $watcher_addr = $_.address
+Write-Output ("Watchlist length: " + $watcherlist.count)
+
+$watcherlist | Where-Object {
+    return $_.enabled
+} | ForEach-Object {
+    [string]$watcher_addr = $_.address
     $watchlist = $_.watchlist
-    
+
+    Write-Output ("Processing " + -join $watcher_addr[0..4] + "...")
+
     $text
 
-    $watchlist | % {
+    $watchlist | ForEach-Object {
         $Name = $_.product
         [int]$Value = $_.price
 
         [int]$actualprice = $prices[$Name]
         if ($prices.ContainsKey($Name) -and $actualprice -lt $Value -and $actualprice -ne 0) {
             $text += "$Name for $actualprice (<$Value)
-            "
+"
         }
     }
 
     if (-not $text -eq '') {
-        curl --user "api:$MAILGUN_API_KEY" https://api.eu.mailgun.net/v3/vikt0r.eu/messages -F from='Scraper <Scraper@vikt0r.eu>' -F to="$watcher_addr" -F subject='Watch notification' -F text="$text"
+        Write-Output "Sending: "
+        Write-Output $text
+        if ($send_email) {
+            curl -s --user "api:$MAILGUN_API_KEY" https://api.eu.mailgun.net/v3/vikt0r.eu/messages -F from='Scraper <Scraper@vikt0r.eu>' -F to="$watcher_addr" -F subject='Watch notification' -F text="$text"
+        }
     }
 }
